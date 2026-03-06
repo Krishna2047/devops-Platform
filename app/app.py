3a62a54032ac7ecf6a08fe2ddcb132a13b129904
Krishna2047 <krishnaaa2047@gmail.com>
2026-03-06 16:20:48 +0530

    Added auto versioning and CI/CD

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"""
+    DevOps Platform Running 🚀
+    Version: {os.getenv("APP_VERSION","v1")}
+    Environment: {os.getenv("APP_ENV","dev")}
+    """
+
+@app.route("/health")
+def health():
+    return "OK"
+
+app.run(host="0.0.0.0", port=5000)
